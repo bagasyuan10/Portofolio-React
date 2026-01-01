1e186f40a41ee77c2d6398c045d24ff8ca2173cd
bagasyuan10 <bagasyuan10@gmail.com>
2026-01-01 12:49:54 +0700

    New UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
